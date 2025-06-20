7875f6e36e3f8ac8533f02ecf3a8a966db8d71f4
mklai10 <mklai10@hotmail.com>
2025-06-19 22:36:11 -0700

    changed tab navigator to use MatrialTopTabs so that it is swipable

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,31 +1,53 @@
+// import { Tabs } from "expo-router";
 import Entypo from '@expo/vector-icons/Entypo';
 import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
-import { Tabs } from "expo-router";
+import {
+  MaterialTopTabNavigationEventMap, MaterialTopTabNavigationOptions, createMaterialTopTabNavigator
+} from "@react-navigation/material-top-tabs";
+import { ParamListBase, TabNavigationState } from "@react-navigation/native";
+import { withLayoutContext } from 'expo-router';
 import { useColorScheme } from "react-native";
+
+const {Navigator} = createMaterialTopTabNavigator();
+
+export const MaterialTopTabs = withLayoutContext<
+  MaterialTopTabNavigationOptions,
+  typeof Navigator,
+  TabNavigationState<ParamListBase>,
+  MaterialTopTabNavigationEventMap
+>(Navigator); 
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
 
+
   return (
-    <Tabs screenOptions={{headerShown: false}}>
-      <Tabs.Screen
-          name="index"
-          options={{
-            tabBarShowLabel: false,
-            tabBarIcon: () => (
-              <Entypo name="home" size={30} color="white" />
-            )
-          }}
-        />
-      <Tabs.Screen
-          name="excercises"
-          options={{
-            tabBarShowLabel: false,
-            tabBarIcon: () => (
-              <FontAwesome5 name="dumbbell" size={24} color="white" />
-            )
-          }}
-        />
-    </Tabs>
+    <MaterialTopTabs
+      tabBarPosition="bottom"
+      style={{marginBottom: 20}}
+    >
+      <MaterialTopTabs.Screen 
+        name="index"
+        options={{
+          tabBarActiveTintColor: 'white',
+          tabBarShowLabel: false,
+          tabBarIndicatorStyle: {opacity: 0},
+          tabBarIcon: ({color}) => (
+            <Entypo name="home" size={30} color={color} />
+          )
+        }}
+      />
+      <MaterialTopTabs.Screen 
+        name="excercises"
+        options={{
+          tabBarActiveTintColor: 'white',
+          tabBarShowLabel: false,
+          tabBarIndicatorStyle: {opacity: 0},
+          tabBarIcon: ({color}) => (
+            <FontAwesome5 name="dumbbell" size={30} color={color} />
+          )
+        }}
+      />
+    </MaterialTopTabs>
   );
 }
